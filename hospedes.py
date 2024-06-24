47c99f4ef337b226b9a661596ee9b5a7eccf12a0
gaoliveira93 <168578360+gaoliveira93@users.noreply.github.com>
2024-06-24 11:13:22 -0300

    Go

---
+++ hospedes.py
@@ -0,0 +1,10 @@
+person = int(input('Digite quantas pessoas:'))
+persons = []
+
+for p in range(person):
+    nome = input('Digite o nome: ')
+    cpf = input('Digite o CPF: ')
+    hospede = [{nome}, 'CPF: ' {cpf}]
+    persons.append(hospede)
+
+print(persons)
