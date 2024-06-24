d5c55fa6b5f5b35e97fae15f0910c6f93cfed971
gaoliveira93 <168578360+gaoliveira93@users.noreply.github.com>
2024-06-24 11:37:28 -0300

    Hospedes

--- hospedes.py
+++ hospedes.py
@@ -4,7 +4,7 @@ persons = []
 for p in range(person):
     nome = input('Digite o nome: ')
     cpf = input('Digite o CPF: ')
-    hospede = [{nome}, 'CPF: ' {cpf}]
+    hospede = [nome, 'CPF:' + cpf]
     persons.append(hospede)
-
+    
 print(persons)
