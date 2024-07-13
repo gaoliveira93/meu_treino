399876bd58d212ee8c5da6a03d63e622570e3b3e
gaoliveira93 <168578360+gaoliveira93@users.noreply.github.com>
2024-07-13 19:01:18 -0300

    novo

--- NovoProjeto.py
+++ NovoProjeto.py
@@ -1,199 +1,77 @@
 import customtkinter
 import tkinter as tk
 from tkinter import ttk
+import pandas as pd
+import numpy
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("dark-blue")
+
+index = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','30','40','50']
+columns = ['Espaço TB', 'ANUAL(U$)', 'MENSAL(U$)', 'ANUAL(R$)', 'MENSAL(R$)', 'Taxa % Cyclopay', 'IOF %', 'Taxa fixa Cyclopay', 'Impostos', 'Custo servidor', 'Custo NF', 'Margem', 'VENDA MENSAL (R$)', 'MENSAL câmbio cartão', 'VALOR (Dólar)']
 
 class CustomWidgets:
     def __init__(self, master, exclude_widgets=[]):
         self.master = master
         self.exclude_widgets = exclude_widgets
+        self.entries = {}
         self.create_widgets()
         
     def create_widgets(self):
-        # Cyclopay_Tax Entry and Label
-        if 'Cyclopay_Tax' not in self.exclude_widgets:
-            self.Cyclopay_Tax = customtkinter.CTkEntry(master=self.master,
-                                                       width=150,
-                                                       height=40,
-                                                       border_width=2,
-                                                       justify='c')
-            self.Cyclopay_Tax.place(x=750, y=170)
-            
-            self.Cyclopay_Tax_Label = customtkinter.CTkLabel(master=self.master,
-                                                             width=10,
-                                                             height=10,
-                                                             text='Taxa Cyclopay :',
-                                                             fg_color='#292929',
-                                                             corner_radius=0)
-            self.Cyclopay_Tax_Label.place(x=620, y=180)
-        
-        # Initial_Tax Entry and Label
-        if 'Initial_Tax' not in self.exclude_widgets:
-            self.Initial_Tax = customtkinter.CTkEntry(master=self.master,
-                                                      width=150,
-                                                      height=40,
-                                                      border_width=2,
-                                                      corner_radius=10,
-                                                      justify='c')
-            self.Initial_Tax.place(x=450, y=30)
-            
-            self.Initial_Tax_Label = customtkinter.CTkLabel(master=self.master,
-                                                            width=10,
-                                                            height=10,
-                                                            text='Imposto :',
-                                                            fg_color='#292929',
-                                                            corner_radius=0)
-            self.Initial_Tax_Label.place(x=310, y=40)
-        
-        # Cost_Tera Entry and Label
-        if 'Cost_Tera' not in self.exclude_widgets:
-            self.Cost_Tera = customtkinter.CTkEntry(master=self.master,
-                                                    width=150,
-                                                    height=40,
-                                                    border_width=2,
-                                                    justify='c')
-            self.Cost_Tera.place(x=750, y=30)
-            
-            self.Cost_Tera_Label = customtkinter.CTkLabel(master=self.master,
-                                                          width=10,
-                                                          height=10,
-                                                          text='Custo por Terabyte :',
-                                                          fg_color='#292929',
-                                                          corner_radius=0)
-            self.Cost_Tera_Label.place(x=620, y=40)
-        
-        # Cost_Invoice Entry and Label
-        if 'Cost_Invoice' not in self.exclude_widgets:
-            self.Cost_Invoice = customtkinter.CTkEntry(master=self.master,
-                                                       width=150,
-                                                       height=40,
-                                                       border_width=2,
-                                                       justify='c')
-            self.Cost_Invoice.place(x=750, y=100)
-            
-            self.Cost_Invoice_Label = customtkinter.CTkLabel(master=self.master,
-                                                             width=10,
-                                                             height=10,
-                                                             text='Custo Nota Fiscal :',
-                                                             fg_color='#292929',
-                                                             corner_radius=0)
-            self.Cost_Invoice_Label.place(x=620, y=110)
-        
-        # Server_Cost Entry and Label
-        if 'Server_Cost' not in self.exclude_widgets:
-            self.Server_Cost = customtkinter.CTkEntry(master=self.master,
-                                                      width=150,
-                                                      height=40,
-                                                      border_width=2,
-                                                      justify='c')
-            self.Server_Cost.place(x=130, y=30)
-            
-            self.Server_Cost_Label = customtkinter.CTkLabel(master=self.master,
-                                                            width=10,
-                                                            height=10,
-                                                            text='Custo Servidor :',
-                                                            fg_color='#292929',
-                                                            corner_radius=0)
-            self.Server_Cost_Label.place(x=20, y=40)
-        
-        # Dolar_Price Entry and Label
-        if 'Dolar_Price' not in self.exclude_widgets:
-            self.Dolar_Price = customtkinter.CTkEntry(master=self.master,
-                                                      width=150,
-                                                      height=40,
-                                                      border_width=2,
-                                                      justify='c')
-            self.Dolar_Price.place(x=130, y=100)
-            
-            self.Dolar_Price_Label = customtkinter.CTkLabel(master=self.master,
-                                                            width=10,
-                                                            height=10,
-                                                            text='Câmbio do dólar :',
-                                                            fg_color='#292929',
-                                                            corner_radius=0)
-            self.Dolar_Price_Label.place(x=15, y=110)
-        
-        # Margim Entry and Label
-        if 'Margim' not in self.exclude_widgets:
-            self.Margim = customtkinter.CTkEntry(master=self.master,
-                                                 width=150,
-                                                 height=40,
-                                                 border_width=2,
-                                                 justify='c')
-            self.Margim.place(x=130, y=170)
-            
-            self.Margim_Label = customtkinter.CTkLabel(master=self.master,
-                                                       width=10,
-                                                       height=10,
-                                                       text='Margem de Lucro :',
-                                                       fg_color='#292929',
-                                                       corner_radius=0)
-            self.Margim_Label.place(x=15, y=180)
-        
-        # IOF Entry and Label
-        if 'IOF' not in self.exclude_widgets:
-            self.IOF = customtkinter.CTkEntry(master=self.master,
-                                              width=150,
-                                              height=40,
-                                              border_width=2,
-                                              corner_radius=10,
-                                              justify='c')
-            self.IOF.place(x=450, y=100)
-            
-            self.IOF_Label = customtkinter.CTkLabel(master=self.master,
-                                                    width=10,
-                                                    height=10,
-                                                    text='IOF :',
-                                                    fg_color='#292929',
-                                                    corner_radius=0)
-            self.IOF_Label.place(x=310, y=110)
-        
-        # Fix_Tax_Cyclopay Entry and Label
-        if 'Fix_Tax_Cyclopay' not in self.exclude_widgets:
-            self.Fix_Tax_Cyclopay = customtkinter.CTkEntry(master=self.master,
-                                                           width=150,
-                                                           height=40,
-                                                           border_width=2,
-                                                           corner_radius=10,
-                                                           justify='c')
-            self.Fix_Tax_Cyclopay.place(x=450, y=170)
-            
-            self.Fix_Tax_Cyclopay_Label = customtkinter.CTkLabel(master=self.master,
-                                                                 width=10,
-                                                                 height=10,
-                                                                 text='Taxa Fixa Cyclopay :',
-                                                                 fg_color='#292929',
-                                                                 corner_radius=0)
-            self.Fix_Tax_Cyclopay_Label.place(x=310, y=180)
+        widget_params = [
+            ('Cyclopay_Tax', 'Taxa Cyclopay :', 750, 170, 620, 180),
+            ('Initial_Tax', 'Imposto :', 450, 30, 310, 40),
+            ('Cost_Tera', 'Custo por Terabyte :', 750, 30, 620, 40),
+            ('Cost_Invoice', 'Custo Nota Fiscal :', 750, 100, 620, 110),
+            ('Server_Cost', 'Custo Servidor :', 130, 30, 20, 40),
+            ('Dolar_Price', 'Câmbio do dólar :', 130, 100, 15, 110),
+            ('Margim', 'Margem de Lucro :', 130, 170, 15, 180),
+            ('IOF', 'IOF :', 450, 100, 310, 110),
+            ('Fix_Tax_Cyclopay', 'Taxa Fixa Cyclopay :', 450, 170, 310, 180)
+        ]
+
+        for key, label, x_entry, y_entry, x_label, y_label in widget_params:
+            if key not in self.exclude_widgets:
+                self.entries[key] = customtkinter.CTkEntry(master=self.master, width=150, height=40, border_width=2, justify='c')
+                self.entries[key].place(x=x_entry, y=y_entry)
+                label_widget = customtkinter.CTkLabel(master=self.master, width=10, height=10, text=label, fg_color='#292929', corner_radius=0)
+                label_widget.place(x=x_label, y=y_label)
         
         # Record Button
         if 'Record_Button' not in self.exclude_widgets:
-            self.Record_Button = customtkinter.CTkButton(master=self.master,
-                                                         width=150,
-                                                         height=40,
-                                                         border_width=2,
-                                                         text='Gravar',
-                                                         compound='bottom')
+            self.Record_Button = customtkinter.CTkButton(master=self.master, width=150, height=40, border_width=2, text='Gravar', command=self.generate_table)
             self.Record_Button.place(x=950, y=100)
         
         # Clean Button
         if 'Clean_Button' not in self.exclude_widgets:
-            self.Clean_Button = customtkinter.CTkButton(master=self.master,
-                                                        width=150,
-                                                        height=40,
-                                                        border_width=2,
-                                                        text='Limpar',
-                                                        compound='bottom',
-                                                        fg_color='#9C0908')
+            self.Clean_Button = customtkinter.CTkButton(master=self.master, width=150, height=40, border_width=2, text='Limpar', compound='bottom', fg_color='#9C0908')
             self.Clean_Button.place(x=950, y=30)
+
+    def generate_table(self):
+        data_dict = {key: self.entries[key].get() for key in self.entries}
+        self.data.loc[len(self.data)] = data_dict
+        self.update_table()
+
+    def update_table(self):
+        for row in self.treeview.get_children():
+            self.treeview.delete(row)
+        for index, row in self.data.iterrows():
+            self.treeview.insert('', 'end', values=row.tolist())
+
+    def create_table(self):
+        self.data = pd.DataFrame(columns=columns, index=index)
+        self.treeview = ttk.Treeview(self.master, columns=columns, show="headings")
+
+        for col in columns:
+            self.treeview.heading(col, text=col)
+            self.treeview.column(col, width=98, stretch=tk.NO, minwidth=50, anchor='center')
+        
+        self.treeview.place(x=10, y=350, width=2000, height=400) 
 
 # Exemplo de uso:
 app = customtkinter.CTk()
 app.title('Calculadora de Custo')
 app.geometry('1200x600')
-app.state('zoomed')
 
 my_tab = customtkinter.CTkTabview(app, height=800, width=800)
 my_tab.pack(side="top", fill="x", pady=10)
@@ -211,9 +89,10 @@ frame2.pack(fill='both', expand=True)
 
 # Instancia os widgets personalizados no frame1
 custom_widgets1 = CustomWidgets(frame1)
+custom_widgets1.create_table()
 
 # Instancia os widgets personalizados no frame2, excluindo alguns widgets
 custom_widgets2 = CustomWidgets(frame2, exclude_widgets=['Cyclopay_Tax', 'Cost_Invoice'])
-#CustomWidgets(frame2, 'frame2', widget_positions={'Server_Cost': (130, 30), 'Server_Cost_Label: (20, 40)})
+custom_widgets2.create_table()
 
 app.mainloop()
--- tryexceptelse.py
+++ tryexceptelse.py
@@ -1,7 +1,64 @@
-try:
-    valor = int(input('Digite um número: '))
-except ValueError:
-    print('Valor incorreto, digite um número: ')
-else:
-    resultado = valor * 2
-    print(resultado)
+import tkinter as tk
+from tkinter import ttk
+import pandas as pd
+
+class App(tk.Tk):
+def create_table(self, app):
+    
+    num_rows = len(index)
+    num_cols = len(columns) 
+
+    cost_tera = float(self.custom_widgets.Cost_Tera.get())
+    initial_tax = float(self.custom_widgets.Initial_Tax.get())
+    cost_invoice = float(self.custom_widgets.Cost_Invoice.get())
+    server_cost = float(self.custom_widgets.Server_Cost.get())
+    cyclopay_tax = float(self.custom_widgets.Cyclopay_Tax.get()) 
+    Dolar_Price = float(self.custom_widgets.Dolar_Price.get())
+    IOF = float(self.custom_widgets.IOF.get())
+    Fix_Tax_Cyclopay = float(self.custom.widgets.Fix_Tax_Cyclopay.get())
+    Tax = month_RS * float(self.custom_widgets.initial_tax.get())
+    server_cost = server_cost
+    Margin = float(self.custom_widgets.Margim.get())
+
+    anual_US = cost_tera * (1-0.15)
+    month_US = anual_US/12
+    anual_RS = anual_US * Dolar_Price
+    month_RS = anual_RS * Dolar_Price
+    cyclopay_taxx = month_RS * cyclopay_tax
+    IOFF = month_RS * IOF
+    month_selling_price = (month_RS + cyclopay_taxx + IOFF + Fix_Tax_Cyclopay + Tax + server_cost + cost_invoice + Margin)
+    month_selling_price_credit = month_selling_price * 1.065
+    selling_dolar_price = month_selling_price_credit * Dolar_Price
+    
+
+    self.df = pd.DataFrame(index=range(num_rows), columns=range(num_cols))
+    for i in range(num_cols):
+        for j in range(num_rows):
+            if j == 1:
+                self.df.iloc[i,j] = anual_US
+            elif j == 2:
+                self.df.iloc[i,j] = month_US
+            elif j == 3:
+                self.df.iloc[i,j] = anual_RS
+            elif j == 4:
+                self.df.iloc[i,j] = month_RS
+            elif j == 5:
+                self.df.iloc[i,j] = cyclopay_taxx
+            elif j == 6:
+                self.df.iloc[i,j] = IOFF
+            elif j == 7:
+                self.df.iloc[i,j] = Fix_Tax_Cyclopay
+            elif j == 8:
+                self.df.iloc[i,j] = Tax
+            elif j == 9:
+                self.df.iloc[i,j] = server_cost
+            elif j == 10:
+                self.df.iloc[i,j] = cost_invoice
+            elif j == 11:
+                self.df.iloc[i,j] = Margin
+            elif j == 12:
+                self.df.iloc[i,j] = month_selling_price
+            elif j == 13:
+                self.df.iloc[i,j] = month_selling_price_credit
+            elif j == 14:
+                self.df.iloc[i,j] = selling_dolar_price
