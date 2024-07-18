db5a17fa33890ee78a788f451728f69e598d3186
gaoliveira93 <168578360+gaoliveira93@users.noreply.github.com>
2024-07-18 11:54:43 -0300

    Ajuste

--- NovoProjeto.py
+++ NovoProjeto.py
@@ -32,7 +32,7 @@ class CustomWidgets:
 
         for key, label, x_entry, y_entry, x_label, y_label in widget_params:
             if key not in self.exclude_widgets:
-                self.entries[key] = customtkinter.CTkEntry(master=self.master, width=150, height=40, border_width=2, justify='c')
+                self.entries[key] = customtkinter.CTkEntry(master=self.master, width=150, height=40, border_width=2, justify='center')
                 self.entries[key].place(x=x_entry, y=y_entry)
                 label_widget = customtkinter.CTkLabel(master=self.master, width=10, height=10, text=label, fg_color='#292929', corner_radius=0)
                 label_widget.place(x=x_label, y=y_label)
@@ -52,8 +52,6 @@ class CustomWidgets:
             self.Record_Button = customtkinter.CTkButton(master=self.master, width=150, height=40, border_width=2, text='Gravar', compound='bottom')
             self.Record_Button.place(x=950, y=170)
 
-
-            
     def update_table(self):
         for row in self.treeview.get_children():
             self.treeview.delete(row)
@@ -73,57 +71,58 @@ class CustomWidgets:
     def generate_table(self):
         Teras = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 30.0, 40.0, 50.0]
 
-        Anual_USD = float(self.entries['Cost_Tera'].get())
-        Value_dolar = float(self.entries['Dolar_Price'].get())
-        Month_USD = float(Anual_USD)
-        Anual_Brl = float(Anual_USD * Value_dolar)
-        Month_Brl = float(Month_USD * Anual_USD)
-        Cyclopay_Tax = float((self.entries['Cyclopay_Tax'].get()))
-        IOFF = float((self.entries['IOF'].get()))
-        Fix_Tax_Cyclopay = float(self.entries['Fix_Tax_Cyclopay'].get())
-        Initial_Tax = float((self.entries['Initial_Tax'].get()))
-        Server_Cost = float(self.entries['Server_Cost'].get())
-        Cost_Invoice = float(self.entries['Cost_Invoice'].get())
-        #Margim = float(((Month_Brl + Cyclopay_Tax + IOFF + Fix_Tax_Cyclopay + Initial_Tax + Server_Cost + Cost_Invoice)/1-self.entries['Margim'].get()) / (Month_Brl + Cyclopay_Tax + IOFF + Fix_Tax_Cyclopay + Initial_Tax + Server_Cost + Cost_Invoice))
-        Month_Brl_CC = float(Month_Brl)
-        Cyclopay_Tax = float((self.entries['Cyclopay_Tax'].get()))
-        Month_USD_CC = float(Month_Brl_CC * Value_dolar)
+        try:
+            Anual_USD = round(float(self.entries['Cost_Tera'].get()), 2)
+            Value_dolar = round(float(self.entries['Dolar_Price'].get()), 2)
+            Cyclopay_Tax = round(float(self.entries['Cyclopay_Tax'].get()), 2)
+            IOFF = round(float(self.entries['IOF'].get()), 2)
+            Fix_Tax_Cyclopay = round(float(self.entries['Fix_Tax_Cyclopay'].get()), 2)
+            Initial_Tax = round(float(self.entries['Initial_Tax'].get()), 2)
+            Server_Cost = round(float(self.entries['Server_Cost'].get()), 2)
+            Cost_Invoice = round(float(self.entries['Cost_Invoice'].get()), 2)
+            Month_USD = round((Anual_USD / 12), 2)
+            Anual_Brl = round((Anual_USD * Value_dolar), 2)
+            Month_Brl = round((Month_USD * Value_dolar), 2)
+            Month_Brl_CC = round((Month_Brl * 1.065), 2)
+            Month_USD_CC = round((Month_Brl_CC / Value_dolar), 2)
 
-        result_Anual_USD = np.array([t * (Anual_USD * 0.85) for t in Teras])
-        result_Value_dolar = np.array([t * Value_dolar for t in Teras])
-        result_Month_USD = np.array([t * (Month_USD / 12) for t in Teras])
-        result_Anual_Brl = np.array([t * Anual_Brl for t in Teras])
-        result_Cyclopay_Tax = np.array([t * (Cyclopay_Tax * Month_Brl) for t in Teras])
-        result_IOFF = np.array([t * (IOFF * Month_USD) for t in Teras])
-        result_Fix_Tax_Cyclopay = np.array([t * Fix_Tax_Cyclopay for t in Teras])
-        result_Initial_Tax = np.array([t * (Initial_Tax * Month_Brl) for t in Teras])
-        result_Server_Cost = np.array([t * Server_Cost for t in Teras])
-        result_Cost_Invoice = np.array([t * Cost_Invoice for t in Teras])
-        #result_Margim = np.array([t * Margim for t in Teras])
-        result_Month_Brl = np.array([t * Month_Brl for t in Teras])
-        result_Month_Brl_CC = np.array([t * (Month_Brl_CC * 1.065) for t in Teras])
-        result_Month_USD_CC = np.array([t * Month_USD_CC for t in Teras])
+            result_Anual_USD = np.array([t * (Anual_USD * 0.85) for t in Teras])
+            result_Value_dolar = np.array([t * Value_dolar for t in Teras])
+            result_Month_USD = np.array([t * Month_USD for t in Teras])
+            result_Anual_Brl = np.array([t * Anual_Brl for t in Teras])
+            result_Cyclopay_Tax = np.array([t * (Cyclopay_Tax * Month_Brl) for t in Teras])
+            result_IOFF = np.array([t * (IOFF * Month_USD) for t in Teras])
+            result_Fix_Tax_Cyclopay = np.array([t * Fix_Tax_Cyclopay for t in Teras])
+            result_Initial_Tax = np.array([t * (Initial_Tax * Month_Brl) for t in Teras])
+            result_Server_Cost = np.array([t * Server_Cost for t in Teras])
+            result_Cost_Invoice = np.array([t * Cost_Invoice for t in Teras])
+            result_Month_Brl = np.array([t * Month_Brl for t in Teras])
+            result_Month_Brl_CC = np.array([t * Month_Brl_CC for t in Teras])
+            result_Month_USD_CC = np.array([t * Month_USD_CC for t in Teras])
 
-        for i in range(len(Teras)):
-            self.treeview.insert('', 'end', text=i, values=(
-                Teras[i],
-                result_Anual_USD[i],
-                result_Value_dolar[i],
-                result_Month_USD[i],
-                result_Anual_Brl[i],
-                result_Cyclopay_Tax[i],
-                result_IOFF[i],
-                result_Fix_Tax_Cyclopay[i],
-                result_Initial_Tax[i],
-                result_Server_Cost[i],
-                result_Cost_Invoice[i],
-               # result_Margim[i],
-                result_Month_Brl[i],
-                result_Month_Brl_CC[i],
-                result_Month_USD_CC[i]
-                ))
-        self.update_table()
+            self.data = pd.DataFrame({
+                'Espaço TB': Teras,
+                'ANUAL(U$)': result_Anual_USD,
+                'MENSAL(U$)': result_Month_USD,
+                'ANUAL(R$)': result_Anual_Brl,
+                'MENSAL(R$)': result_Month_Brl,
+                'Taxa % Cyclopay': result_Cyclopay_Tax,
+                'IOF %': result_IOFF,
+                'Taxa fixa Cyclopay': result_Fix_Tax_Cyclopay,
+                'Impostos': result_Initial_Tax,
+                'Custo servidor': result_Server_Cost,
+                'Custo NF': result_Cost_Invoice,
+                'Margem': 0,  # Você pode adicionar a lógica de margem aqui
+                'VENDA MENSAL (R$)': result_Month_Brl,
+                'MENSAL câmbio cartão': result_Month_Brl_CC,
+                'VALOR (Dólar)': result_Month_USD_CC
+            })
 
+            self.update_table()
+
+        except ValueError as e:
+            print(f"Erro de conversão: {e}")
+            # Você pode adicionar aqui uma mensagem de erro no aplicativo tkinter se desejar
 
 # Exemplo de uso:
 app = customtkinter.CTk()
