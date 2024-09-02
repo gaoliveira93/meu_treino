1bc06ebb28b14293ecf7b47be9f04a67e58c111f
gaoliveira93 <168578360+gaoliveira93@users.noreply.github.com>
2024-09-02 15:01:37 -0300

    att

--- tryexceptelse.py
+++ tryexceptelse.py
@@ -1,4 +1,5 @@
 import pyautogui
+import time
 
 #pythonautogui.write()
 #pythonautogui.click()
@@ -10,5 +11,25 @@ pyautogui.PAUSE = 1
 
 pyautogui.hotkey('win')
 pyautogui.write('brave')
-x, y, largura, altura = pyautogui.locateCenterOnScreen('Brave.png')
-pyautogui.click(x + largura/2, y + altura/2)
+x, y = pyautogui.locateCenterOnScreen('Brave.png', confidence=0.7)
+pyautogui.click(x, y)
+pyautogui.write('gmail.com')
+pyautogui.press('Enter')
+while True:
+    try:
+        location = pyautogui.locateOnScreen('Barra_Email.png')
+        if location is not None:
+            x, y = location
+            pyautogui.click(x, y)
+            break  # Exit loop once the image is found and clicked
+        else:
+            print('Image not found, trying again...')
+            time.sleep(1)  # Wait before retrying to reduce resource usage
+    except pyautogui.ImageNotFoundException:
+        print('Image not found exception raised, trying again...')
+        time.sleep(1)
+
+x, y = pyautogui.locateCenterOnScreen('Menu.png', confidence=0.7)
+pyautogui.click(x, y)
+x, y = pyautogui.locateCenterOnScreen('Contatos.png', confidence=0.7)
+pyautogui.click(x, y)
